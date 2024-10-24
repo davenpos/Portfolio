d9140ce17db79c9c7884df6c65794dfba5979bb3
davenpos <davenpos@uwindsor.ca>
2024-10-24 19:54:19 -0400

    Fixed bug with wrong menu link lighting up after being on project page

--- components/MenuLink.jsx
+++ components/MenuLink.jsx
@@ -4,7 +4,7 @@ import { usePathname } from 'next/navigation'
 
 export default function MenuLink({href, name}) {
     const pathname = usePathname()
-    const currPage = pathname === href || (window.location.href.includes(href) && href != "/")
+    const currPage = pathname === href || (pathname.startsWith(href) && href != "/")
 
     return <Link className={`p-2 duration-300 ${currPage ? "text-slate-950 underline font-bold" : "hover:opacity-50"}`} href={href}>{name}</Link>
 }
