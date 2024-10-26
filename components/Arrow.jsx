3c071da51b18459cc74b5376c3c0f40c072a343f
davenpos <davenpos@uwindsor.ca>
2024-10-25 20:26:14 -0400

    Tried changing slide show view for project pages to Glide (may scrap this entirely)

--- components/Arrow.jsx
+++ components/Arrow.jsx
@@ -1,6 +1,6 @@
-export default function Arrow({dir, handler}) {
-    const id = dir === "left" ? "prevSS" : "nextSS"
+export default function Arrow({dir}) {
     const arrow = dir === "left" ? "\u276e" : "\u276f"
+    const id = (dir === "left" ? "prev" : "next") + "Arrow"
 
-    return <a id={id} onClick={handler} className={`absolute top-1/2 mt-[-14px] text-2xl font-bold p-3 cursor-pointer duration-300 opacity-0 group-hover:opacity-100 hover:text-white hover:bg-black/80 ${dir === "left" ? "hidden left-0 rounded-r-[5px]" : "right-0 rounded-l-[5px]"}`}>{arrow}</a>
+    return <button id={id} className={`absolute top-1/2 mt-[14px] text-2xl font-bold p-3 cursor-pointer duration-300 opacity-0 group-hover:opacity-100 hover:text-white hover:bg-black/80 glide__arrow glide__arrow--${dir} ${dir === "left" ? "rounded-r-[5px]" : "rounded-l-[5px] left-[971px]"}`} data-glide-dir={`${dir === "left" ? "<" : ">"}`}>{arrow}</button>
 }
--- components/ScreenshotsSlide.jsx
+++ components/ScreenshotsSlide.jsx
@@ -1,32 +1,28 @@
 "use client"
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
+import Glide from '@glidejs/glide'
 import Arrow from '@/components/Arrow'
-import nextScreenshot from '@/functions/nextScreenshot'
 const dotenv = require('dotenv')
 dotenv.config()
 
 export default function ScreenshotsSlide({title, ss}) {
-    const [currIndex, setCurrIndex] = useState(0)
+    useEffect(() => {
+        new Glide("#screenshots").mount()
+    }, [])
 
-    const prevSlide = () => {
-        let newIndex = currIndex - 1
-        setCurrIndex(currIndex - 1)
-        nextScreenshot(currIndex, newIndex, ss.length)
-    }
-
-    const nextSlide = () => {
-        let newIndex = currIndex + 1
-        setCurrIndex(currIndex + 1)
-        nextScreenshot(currIndex, newIndex, ss.length)
-    }
-
-    return (<div className="relative group">
-        {ss.map((curr, i) => {
-            return <img id={`screenshot${i}`} key={curr.id} src={`${process.env.NEXT_PUBLIC_STRAPIURL}${curr.formats.large.url}`} alt={`Screenshot ${i + 1} of ${title}`} className={`w-full ${i > 0 ? "hidden" : ""}`} />
-        })}
-        {ss.length > 1 ? <>
-            <Arrow dir="left" handler={prevSlide} />
-            <Arrow dir="right" handler={nextSlide} />
-        </> : null}
+    return (<div id="screenshots" className="overflow-hidden group">
+        <div className="glide__track" data-glide-el="track">
+            <div className="glide__slides">
+                {ss.map((curr, i) => (
+                    <img id={`screenshot${i}`} key={curr.id} src={`${process.env.NEXT_PUBLIC_STRAPIURL}${curr.formats.large.url}`} alt={`Screenshot ${i + 1} of ${title}`} className="w-full inline" />
+                ))}
+            </div>
+            {ss.length > 1 ? <>
+                <div className="glide__arrows" data-glide-el="controls">
+                    <Arrow dir="left" />
+                    <Arrow dir="right" />
+                </div>
+            </> : null}
+        </div>
     </div>)
 }
