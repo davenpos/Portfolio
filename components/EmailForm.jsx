5d959c37dd1130ad78ad0b42b8c2dd74c871a2ba
davenpos <davenpos@uwindsor.ca>
2024-10-29 11:19:21 -0400

    Made some minor improvements on the email form

--- components/EmailForm.jsx
+++ components/EmailForm.jsx
@@ -1,9 +1,24 @@
 "use client"
+import { useEffect, useState } from "react";
 const validator = require("validator")
 import FormInput from '@/components/FormInput'
 import sendEmail from '@/functions/sendEmail'
 
 export default function EmailForm() {
+    const [emailLabel, setEmailLabel] = useState("Your email:")
+
+    useEffect(() => {
+        function updateIconSize() {
+            if (window.innerWidth > 768) setEmailLabel("Your email address:")
+            else setEmailLabel("Your email:")
+        }
+
+        updateIconSize()
+
+        window.addEventListener("resize", updateIconSize)
+        return () => window.removeEventListener("resize", updateIconSize)
+    }, [])
+
     const formSubmit = async e => {
         e.preventDefault()
         const fd = new FormData(e.target)
@@ -11,21 +26,24 @@ export default function EmailForm() {
         const subject = fd.get("subject")
         const content = fd.get("content")
         const message = document.getElementById("message")
+        const emailField = document.querySelector("input[name='email']")
+        const subjectField = document.querySelector("input[name='subject']")
+        const contentField = document.querySelector("textarea")
 
         if (fromEmail === "" || typeof(fromEmail) != "string" || !validator.isEmail(fromEmail)) {
-            document.querySelector("input[name='email']").value = ""
+            emailField.value = ""
             message.innerText = "Please enter your email address."
         } else if (subject === "" || typeof(subject) != "string") {
-            document.querySelector("input[name='subject']").value = ""
+            subjectField.value = ""
             message.innerText = "Please enter a subject."
         } else if (content === "" || typeof(content) != "string") {
-            document.querySelector("textarea").value = ""
+            contentField.value = ""
             message.innerText = "Please enter your email content."
         } else {
             await sendEmail(fromEmail, subject, content)
-            document.querySelector("input[name='email']").value = ""
-            document.querySelector("input[name='subject']").value = ""
-            document.querySelector("textarea").value = ""
+            emailField.value = ""
+            subjectField.value = ""
+            contentField.value = ""
             message.innerText = "Your email to Simeon has been sent!"
         }
         message.classList.remove("invisible")
@@ -33,8 +51,8 @@ export default function EmailForm() {
 
     return (<>
         <h3 className="text-2xl font-bold text-center my-3">Email Simeon:</h3>
-        <form onSubmit={formSubmit}>
-            <FormInput name="email" label="Your email address:" />
+        <form onSubmit={formSubmit} className="text-sm md:text-base">
+            <FormInput name="email" label={emailLabel} />
             <FormInput name="subject" label="Subject:" />
             <FormInput name="content" label="Type email content here" />
             <div className="col-span-2 flex justify-items-center content-center">
--- components/FormInput.jsx
+++ components/FormInput.jsx
@@ -2,7 +2,7 @@ export default function FormInput({name, label}) {
     return (<>
         {name === "content" ?
             <textarea name={name} id={name} placeholder={label} rows="15" className="border border-black mb-2 col-span-2 p-1 w-full"></textarea> :
-            <div className="grid grid-cols-[20%_80%] my-2">
+            <div className="grid grid-cols-[25%_75%] md:grid-cols-[20%_80%] my-2">
                 <label htmlFor={name}>{label}</label>
                 <input type={name === "email" ? "email" : "text"} id={name} name={name} className="border border-black px-1" />
             </div>
