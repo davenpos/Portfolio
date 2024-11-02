5dbc89dbe56201cb316a3cf5880a453b4342cb37
davenpos <davenpos@uwindsor.ca>
2024-11-02 15:18:33 -0400

    Made it so that after a message displays, if the user types something into one of the fields, the message immediately disappears

--- components/EmailForm.jsx
+++ components/EmailForm.jsx
@@ -1,11 +1,38 @@
 "use client"
 const validator = require("validator")
+import { useState, useEffect, useRef } from "react"
 import FormInput from '@/components/FormInput'
 import sendEmail from '@/functions/sendEmail'
 import windowSizeState from '@/functions/windowSizeState'
 
 export default function EmailForm() {
-    const emailLabel = windowSizeState("Your email:", "Your email address:");
+    const emailLabel = windowSizeState("Your email:", "Your email address:")
+    const [message, setMessage] = useState("Message")
+    const [messageVisible, setMessageVisible] = useState(false)
+
+    const emailFieldRef = useRef(null)
+    const subjectFieldRef = useRef(null)
+    const contentFieldRef = useRef(null)
+
+    useEffect(() => {
+        const emailField = emailFieldRef.current
+        const subjectField = subjectFieldRef.current
+        const contentField = contentFieldRef.current
+
+        const fieldChange = () => {
+            if (emailField.value || subjectField.value || contentField.value) setMessageVisible(false)
+        }
+
+        emailField?.addEventListener("input", fieldChange)
+        subjectField?.addEventListener("input", fieldChange)
+        contentField?.addEventListener("input", fieldChange)
+
+        return () => {
+            emailField?.removeEventListener("input", fieldChange)
+            subjectField?.removeEventListener("input", fieldChange)
+            contentField?.removeEventListener("input", fieldChange)
+        }
+    }, [])
 
     const formSubmit = async e => {
         e.preventDefault()
@@ -13,40 +40,36 @@ export default function EmailForm() {
         const fromEmail = fd.get("email")
         const subject = fd.get("subject")
         const content = fd.get("content")
-        const message = document.getElementById("message")
-        const emailField = document.querySelector("input[name='email']")
-        const subjectField = document.querySelector("input[name='subject']")
-        const contentField = document.querySelector("textarea")
 
         if (fromEmail === "" || typeof(fromEmail) != "string" || !validator.isEmail(fromEmail)) {
-            emailField.value = ""
-            message.innerText = "Please enter your email address."
+            emailFieldRef.current.value = ""
+            setMessage("Please enter your email address.")
         } else if (subject === "" || typeof(subject) != "string") {
-            subjectField.value = ""
-            message.innerText = "Please enter a subject."
+            subjectFieldRef.current.value = ""
+            setMessage("Please enter a subject.")
         } else if (content === "" || typeof(content) != "string") {
-            contentField.value = ""
-            message.innerText = "Please enter your email content."
+            contentFieldRef.current.value = ""
+            setMessage("Please enter your email content.")
         } else {
             await sendEmail(fromEmail, subject, content)
-            emailField.value = ""
-            subjectField.value = ""
-            contentField.value = ""
-            message.innerText = "Your email to Simeon has been sent!"
+            emailFieldRef.current.value = ""
+            subjectFieldRef.current.value = ""
+            contentFieldRef.current.value = ""
+            setMessage("Your email to Simeon has been sent!")
         }
-        message.classList.remove("invisible")
+        setMessageVisible(true)
     }
 
     return (<>
         <h3 className="text-2xl font-bold text-center my-3">Email Simeon:</h3>
         <form onSubmit={formSubmit} className="text-sm md:text-base">
-            <FormInput name="email" label={emailLabel} />
-            <FormInput name="subject" label="Subject:" />
-            <FormInput name="content" label="Type email content here" />
+            <FormInput name="email" label={emailLabel} ref={emailFieldRef} />
+            <FormInput name="subject" label="Subject:" ref={subjectFieldRef} />
+            <FormInput name="content" label="Type email content here" ref={contentFieldRef} />
             <div className="col-span-2 flex justify-items-center content-center">
                 <input type="submit" className="mx-auto rounded-[15px] bg-orange-500 px-3 py-2 text-white shadow-md mb-2 cursor-pointer" value="Send email" />
             </div>
         </form>
-        <p id="message" className="text-orange-500 text-center font-bold drop-shadow-md invisible">Message</p>
+        <p id="message" className={`text-orange-500 text-center font-bold drop-shadow-md ${messageVisible ? "" : "invisible"}`}>{message}</p>
     </>)
 }
--- components/FormInput.jsx
+++ components/FormInput.jsx
@@ -1,11 +1,13 @@
-export default function FormInput({name, label}) {
+import { forwardRef } from "react"
+
+export default forwardRef(function FormInput({name, label}, ref) {
     return (<>
         {name === "content" ?
-            <textarea name={name} id={name} placeholder={label} rows="15" className="border border-black mb-2 col-span-2 p-1 w-full"></textarea> :
+            <textarea name={name} id={name} placeholder={label} rows="15" className="border border-black mb-2 col-span-2 p-1 w-full" ref={ref}></textarea> :
             <div className="grid grid-cols-[25%_75%] md:grid-cols-[20%_80%] my-2">
                 <label htmlFor={name}>{label}</label>
-                <input type={name === "email" ? "email" : "text"} id={name} name={name} className="border border-black px-1" />
+                <input type={name === "email" ? "email" : "text"} id={name} name={name} className="border border-black px-1" ref={ref} />
             </div>
         }
     </>)
-}
+})
