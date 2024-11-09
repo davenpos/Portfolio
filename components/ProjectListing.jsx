c36ea2262d74c28365d0c8d87dd2c5869be7f317
davenpos <davenpos@uwindsor.ca>
2024-11-09 15:24:33 -0500

    Made a handful of minor improvements to the Projects page, including making it so only clicking the actual text of the title will take you to the individual project page

--- components/ProjectListing.jsx
+++ components/ProjectListing.jsx
@@ -13,10 +13,12 @@ export default function ProjectListing(props) {
         <Link href={`/projects/${props.slug}`} className="overflow-hidden flex-shrink-0 w-[350px]">
             <img src={`${process.env.NEXT_PUBLIC_STRAPIURL}/${props.img}`} className="object-cover hover:opacity-70 duration-300 hover:scale-125 w-full h-auto" alt="Project Image" />
         </Link>
-        <div className="flex flex-col max-w-full w-full overflow-hidden">
-            <Link href={`/projects/${props.slug}`} className="font-bold text-xl hover:opacity-50 duration-300 text-center md:text-left w-full">
-                {props.title}
-            </Link>
+        <div className="flex flex-col max-w-full overflow-hidden">
+            <div>
+                <Link href={`/projects/${props.slug}`} className="font-bold text-xl hover:opacity-50 duration-300 text-center md:text-left">
+                    {props.title}
+                </Link>
+            </div>
             <LanguagesAndStacks arr={props.las} links={langsLinks} divClasses="hide-scrollbars w-full max-w-full overflow-x-auto whitespace-nowrap" />
             <p className="text-sm md:text-base w-full break-words overflow-hidden">
                 {excerpt}
