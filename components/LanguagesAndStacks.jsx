e9a81f6f3290b66ba8906cded392974d19d8e1be
davenpos <davenpos@uwindsor.ca>
2024-11-09 15:02:06 -0500

    Made languages and stacks on project page clickable and displays only projects of that language or stack when clicked on

--- components/LanguagesAndStacks.jsx
+++ components/LanguagesAndStacks.jsx
@@ -1,9 +1,27 @@
+"use client"
+import { usePathname, useSearchParams, useRouter } from 'next/navigation'
 import React from "react"
 
+//http://localhost:1337/api/projects?filters[languages_stacks][slug][$in]=java&populate=*
+
 export default function LanguagesAndStacks({arr, links, divClasses}) {
+    const pathname = usePathname()
+    const searchParams = useSearchParams()
+    const { replace } = useRouter()
+
+    function langOrStackOnly(los) {
+        const params = new URLSearchParams(searchParams)
+        params.set('langOrStack', los)
+        replace(`${pathname}?${params.toString()}`)
+    }
+
     return (<div className={divClasses}>
         {arr.map((los, i) => <React.Fragment key={los.id}>
-            <span className={`bg-black text-white rounded-full p-1 font-mono ${links ? "hover:bg-white hover:text-black duration-300" : "cursor-default"}`}>{los.name}</span>
+            {links ?
+                <span className="bg-black text-white rounded-full p-1 font-mono hover:bg-white hover:text-black duration-300 cursor-pointer" onClick={() => langOrStackOnly(los.slug)}>{los.name}</span>
+                :
+                <span className="bg-black text-white rounded-full p-1 font-mono cursor-default">{los.name}</span>
+            }
             {i < arr.length - 1 ? <span className="cursor-default">&nbsp;</span> : null}
         </React.Fragment>)}
     </div>)
--- functions/getPaginationVars.js
+++ functions/getPaginationVars.js
@@ -6,13 +6,26 @@ export default async function getPaginationVars(props, numPerPage, slug, field) 
     const pageNum = searchParams.page ? parseInt(searchParams.page) : 1
     const start = (pageNum - 1) * numPerPage
 
-    const query = {
+    let query = {
         sort: `${field}:desc`,
         pagination: {
             start: start,
             limit: numPerPage
         }
     }
+
+    const los = searchParams.langOrStack
+
+    if (los) {
+        query.filters = {
+            languages_stacks: {
+                slug: {
+                    $in: los
+                }
+            }
+        }
+    }
+
     const results = await getStrapi(slug, qs.stringify(query), true)
     const total = results.meta.pagination.total
 
