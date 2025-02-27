a211c2b22f83590230dd1c895f7bd975cb67f436
davenpos <davenpos@uwindsor.ca>
2025-02-27 17:33:57 -0500

    Fixed issue where you can't click back to the first pagination page when on the next

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -5,10 +5,12 @@ import useSetQueryParams from '@/hooks/useSetQueryParams'
 export default function Pagination({pages, curr, numPerPage, total}) {
     const setQueryParams = useSetQueryParams()
     const pageNums = []
-    const params = [{ queryVar: "page" }]
 
     for (let i = 1; i <= pages; i++) {
-        params[0].val = i
+        const params = [{
+            queryVar: "page",
+            val: i
+        }]
 
         pageNums.push(<React.Fragment key={i}>
             <span className={`duration-300 ${i === curr ? "text-slate-950 font-bold" : "hover:opacity-50 cursor-pointer underline"}`} onClick={() => setQueryParams(params)}>{i}</span>
