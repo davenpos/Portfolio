0c637bcf2d3f30978724bbe27b59ae010b383d32
davenpos <davenpos@uwindsor.ca>
2025-02-26 19:57:36 -0500

    Edited app/page.tsx, updated CollectionTypeSlug name and converted getEntryInfo.js to TypeScript

--- functions/getStrapi.ts
+++ functions/getStrapi.ts
@@ -1,8 +1,8 @@
-export default async function getStrapi(slug: CollectionTypeSlugs, param: string, includeMeta: true): Promise<StrapiAPI>
+export default async function getStrapi(slug: CollectionTypeSlug, param: string, includeMeta: true): Promise<StrapiAPI>
 
-export default async function getStrapi(slug: CollectionTypeSlugs, param: string, includeMeta?: false): Promise<StrapiEntry[]>
+export default async function getStrapi(slug: CollectionTypeSlug, param: string, includeMeta?: false): Promise<StrapiEntry[]>
 
-export default async function getStrapi(slug: CollectionTypeSlugs, param: string, includeMeta = false): Promise<StrapiEntry[] | StrapiAPI> {
+export default async function getStrapi(slug: CollectionTypeSlug, param: string, includeMeta = false): Promise<StrapiEntry[] | StrapiAPI> {
     let url = `${process.env.NEXT_PUBLIC_STRAPIURL}/api/${slug}?populate=*&${param}`
     const prom = await fetch(url)
     const info: StrapiAPI = await prom.json()
