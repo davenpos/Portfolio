a58d1b455df8ac65874b7d83592df6bc256c8f77
davenpos <davenpos@uwindsor.ca>
2025-03-02 11:10:02 -0500

    Converted about page and getExcerpt function to TypeScript

--- functions/getExcerpt.ts
+++ functions/getExcerpt.ts
@@ -1,4 +1,4 @@
-export default function getExcerpt(desc, words) {
+export default function getExcerpt(desc: string, words: number) {
     const descArray = desc.split(' ')
     return descArray.length < words ? desc : descArray.slice(0, words).join(' ') + "..."
 }
